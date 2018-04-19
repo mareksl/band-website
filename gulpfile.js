888d04eb12b0f3509f0c97056458561ac4797c10
mareksl <marekslad@hotmail.com>
2018-04-19 16:52:17 +0200

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const bs = require('browser-sync').create();
+
+gulp.task('sass', () => {
+  return gulp
+    .src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/css'))
+    .pipe(bs.stream());
+});
+
+gulp.task('bs', ['sass'], () => {
+  bs.init({
+    server: {
+      baseDir: './src/'
+    }
+  });
+
+  gulp.watch('./src/scss/**/*.scss', ['sass']);
+  gulp.watch('./src/*.html').on('change', bs.reload);
+});
+
+gulp.task('default', ['bs']);
